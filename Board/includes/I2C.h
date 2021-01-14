87af14dca80d35d306cef740b2f7dcf67fc06aa8
gianniruf <77435864+gianniruf@users.noreply.github.com>
2021-01-14 19:27:39 +0100

    Update I2C.h
    New Version, not working yet

--- Board/includes/I2C.h
+++ Board/includes/I2C.h
@@ -1,57 +1,106 @@
 /*
- * I2C.h
+ * !!! NOT WORKING YET !!!!
+ */
+
+/*
+ * elob_i2c.h
  *
- * Created: 09.10.2019 10:43:21
- * Author: rufg
- * 
- * Library zum Bedienen der ELOB-I2C-Schnittstelle
- * Interrupts müssen im main() freigegeben werden (sei();)
+ * Created: 17.12.2020 08:44:35
+ * Version	Datum			Name			Change
+ * 1.0		17. Dec 2019	Gianni Ruf		Creation
+ * 2.0		19. Dec 2020	Gianni Ruf		Update
+ *
+ * Description
+ *	Library for the ADC on the ELOB.
+ *	Already done:
+ *		- send
+ *
+ * Hardware
+ *	ELOB (Elektroniker Board, ZLV-Mint)
+ *	Controller: ATMega2560 (16 MHz)
+ *
+ * Requirements from Main:
+ *	- Interrupts
+ *	- optional FIFO (define USE_FIFO)
  */ 
 
 
-#ifndef I2C_H_
-#define I2C_H_
+#ifndef ELOB_I2C_H_
+#define ELOB_I2C_H_
+
+//***INCLUDES***
+#include <avr/io.h>
+#include <stdint.h>
+#include <avr/interrupt.h>
+
 //***DEFINES***
+enum I2C_states{I2C_IDLE = 0, I2C_STARTING, I2C_STARTED, I2C_RESTARTED, I2C_SLV_ADDRESSED, I2C_SLV_REG_ADDRESSED, I2C_WRITE, I2C_READ, I2C_STOPPING, I2C_GOT_ADDRESSED};
+enum I2C_error{I2C_NO_ERR = 0, I2C_INVALID_FUNCTION_PARAM, I2C_GOT_NACK, I2C_BUSY, I2C_MISSING_MEMBER, I2C_UNDEF_ERROR};
+enum I2C_modes{I2C_NO,	I2C_MASTER_R,		I2C_MASTER_W,		I2C_SLAVE_R,		I2C_SLAVE_W};
+//						isMaster, Read		isMaster,write		isSlave, send		isSlave, get
+#define I2C_SIZE_DATABUFFER	10
 
-#define I2C_R	1	//I2C-Lesen (bei Adresse)
-#define I2C_W	0	//I2C-Schreiben (bei Adresse)
+//***i2c-addresses***
+#define I2C_ADDRESS_RTC	0b1101000
 
-//***ARRAYS***
-unsigned char i2c_dataWrite[20];	//Daten (20x 8-Bit) welche gesendet werden
-unsigned char i2c_dataRead[20];		//Daten (20x 8-Bit) welche erhalten wurden
+//***VARIABLEN GLOBAL***
+typedef struct{
+	uint8_t data_slave[I2C_SIZE_DATABUFFER];		//Data Buffer SlaveMode, for Read AND write
+	uint8_t data_master[I2C_SIZE_DATABUFFER];	//Data Buffer MasterMode, for Read AND write
+	uint8_t state;		//state of I2C protocoll
+	uint8_t mode_uart;		//Master/slave, Read/write
+	uint8_t error;
+} elobI2C_t;
 
-//***VARIABLEN***
-unsigned char i2c_writeFinished;	//1 = schreiben beendet
-unsigned char i2c_readFinished;	//1 = lesen beendet
+elobI2C_t I2C;
 
 //***FUNKTIONSPROTOTYPEN***
 
 /**
- * Initialisiert I2C-Schnittstelle
- * @param:	TWBR	0...255 für TWBR- TWI Bit Rate Register
- * @pram:	TWPS	Prescaler Value 1, 4, 16, 64
- * @return:	void
+ * Initialisiert I2C on the ELOB
+ * @param:	bitRate 0..255
+ * @param:	clkPrescaler 1, 4, 16, 64
+ * @param:	slvModeAddress	(0x01..0x7F)
+ * @return:	error
+ *
+ * Standartmode is Slave-Mode
  */
-void I2C_init(unsigned char, unsigned char);	
+uint8_t elobI2C_init(uint8_t bitRate, uint8_t clkPrescaler, uint8_t slvModeAddress);
 
 /**
- * Startet eine I2C-SCHREIB-Kommunikation
- * @param:	SlvAdress	7-Bit
- * @param:	WordAdress	8-Bit
- * @param:	AnzBytes die am Stück geschrieben werden	0..20
- * @return:	void
+ * Start a writing of Data to Slave
+ * @param:		slvAdress		Adress of Slave (7-Bit, lower-Bits)
+ * @param:		slvRegAdress	RegNumber of the first Byte (8-Bit)
+ * @param:		nBytes			number of Bytes to read (1..I2C_SIZE_DATABUFFER)
+ * Databuffer must be filled befor the function-call!!!
+ *
+ * At End the FIFO will get a message if USE_FIFO is defined
+ *
+ * if an ERROR happens, it stops the communication, and set an Value to I2C.error .
+ * The FIFO will get a message if USE_FIFO is defined
+ * It does NOT reset/restart/retry automatically
  */
-void I2C_write_start(unsigned char, unsigned char, unsigned char);
+void elobI2C_masterMod_writeData_start(uint8_t slvAdress, uint8_t slvRegAdress, uint8_t nBytes);
 
 /**
- * Startet eine I2C-LESE-Kommunikation
- * @param:	SlvAdress	7-Bit
- * @param:	WordAdress	8-Bit
- * @param:	AnzBytes die am Stück gelesen werden	0..20
- * @return:	void
+ * Start reading Data from Slave
+ * @param:		slvAdress		Address of Slave (7-Bit, lower-Bits)
+ * @param:		slvRegAdress	RegNumber of the first Byte (8-Bit)
+ * @param:		nBytes			number of Bytes to read (1..I2C_SIZE_DATABUFFER)
+ * Data buffer will be filled
+ *
+ * At End the FIFO will be get a message if USE_FIFO is defined
+ *
+ * if an ERROR happens, it stops the communication, and set an Value to I2C.error .
+ * The FIFO will get a message if USE_FIFO is defined
+ * It does NOT reset/restart/retry automatically
  */
-void I2C_read_start(unsigned char, unsigned char, unsigned char);
+void elobI2C_masterMod_readData_start(uint8_t slvAdress, uint8_t slvRegAdress, uint8_t nBytes);
 
+/**
+ * Reset the I2C
+ * @return: error	(0=no error, else: error)
+ */
+uint8_t elobI2C_reset(void);
 
-
-#endif /* I2C_H_ */
+#endif /* ELOB_I2C_H_ */

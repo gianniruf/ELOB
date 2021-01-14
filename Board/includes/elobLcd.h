6657616dbb93f5d40440347ae273b8c5316912e0
gianniruf <77435864+gianniruf@users.noreply.github.com>
2021-01-14 13:39:32 +0100

    Rename elobLcd.h to Board/includes/elobLcd.h
    New location

--- Board/includes/elobLcd.h
+++ Board/includes/elobLcd.h
@@ -22,12 +22,12 @@ void elobLcd_init(void);
 
 /**
  * Gibt ein Zeichen aus
- * @param	gewünschtes Zeichen in char (mit 'X')
+ * @param	gewÃ¼nschtes Zeichen in char (mit 'X')
  */
 void elobLcd_zeichen(unsigned char);
 
 /**
- * Führt 8-Bit/4-Bit befehl aus
+ * FÃ¼hrt 8-Bit/4-Bit befehl aus
  * @param	Befehl 8Bit
  */
 void elobLcd_instruktion(unsigned char);	//befehl
